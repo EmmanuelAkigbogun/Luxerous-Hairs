4798ff30dcaaba1e6c4366627c5f12e521aa6369
EmmanuelAkigbogun <105459628+EmmanuelAkigbogun@users.noreply.github.com>
2023-12-22 02:51:11 +0100

    Update Dill.jsx

--- src/Dill.jsx
+++ src/Dill.jsx
@@ -1,30 +1,8 @@
-import { useState } from "react";
+
 function Dill() {
-  let [r, q] = useState({ a1: 1, a2: 1, a3: 1 });
   return (
     <>
-      <button
-        onClick={() => {
-          q((r = { a1: r["a1"] + 1, a2: r["a2"], a3: r["a3"] }));
-        }}
-      >
-        light
-      </button>
-      <button
-        onClick={() => {
-          q((r = { a1: r["a1"], a2: r["a2"] + 1, a3: r["a3"] }));
-        }}
-      >
-        hate
-      </button>
-      <button
-        onClick={() => {
-          q((r = { a1: r["a1"], a2: r["a2"], a3: r["a3"] + 1 }));
-        }}
-      >
-        love
-      </button>
-      {console.log(r)}
+  
     </>
   );
 }
